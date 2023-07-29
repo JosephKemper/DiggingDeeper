6c3eac007dfb892f1af813817c2cb14fdfa86c19
JosephKemper <JosephKemper@gmail.com>
2023-07-28 18:44:47 -0600

    Setting up github pages site

---
+++ CapitalizeMethodTest.py
@@ -0,0 +1,3 @@
+example_string = "this is an example of how the capitalize method for strings works in Python."
+print(example_string.capitalize())
+# Expected output This is an example of how the capitalize method for strings works in python.
