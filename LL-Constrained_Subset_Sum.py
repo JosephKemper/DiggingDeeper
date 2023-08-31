968cd351d4e4d878dee5c3de010c1c2adbab167c
Joseph Kemper <JosephKemper@gmail.com>
2023-08-30 20:03:12 -0600

    Studied the Constrained Subset Sum Problem

--- LL-Constrained_Subset_Sum.py
+++ LL-Constrained_Subset_Sum.py
@@ -8,20 +8,20 @@ def constrained_subset_sum(number_list, max_distance):
     # Create a deque to store the indices of the maximum sums
     max_sum_indices = deque()
     # Iterate over each element in the input list
-    for i in range(list_length):
+    for index in range(list_length):
         # Set the current maximum sum to the current element in the input list
-        max_sums[i] = number_list[i]
+        max_sums[index] = number_list[index]
         # If the deque is not empty, add the maximum sum at the first index in the deque to the current maximum sum
         if max_sum_indices:
-            max_sums[i] += max_sums[max_sum_indices[0]]
+            max_sums[index] += max_sums[max_sum_indices[0]]
         # While the first index in the deque is less than i - max_distance, remove it from the deque
-        while max_sum_indices and max_sum_indices[0] < i - max_distance:
+        while max_sum_indices and max_sum_indices[0] < index - max_distance:
             max_sum_indices.popleft()
         # While the last index in the deque has a smaller maximum sum than the current maximum sum, remove it from the deque
-        while max_sum_indices and max_sums[max_sum_indices[-1]] < max_sums[i]:
+        while max_sum_indices and max_sums[max_sum_indices[-1]] < max_sums[index]:
             max_sum_indices.pop()
         # Add the current index to the deque
-        max_sum_indices.append(i)
+        max_sum_indices.append(index)
     # Return the maximum value in the maximum sums list
     return max(max_sums)
 
@@ -31,7 +31,75 @@ max_distance = 2
 print(constrained_subset_sum(number_list, max_distance)) # Output: 37
 
 
+"""
+Sure! Here are the plain links to the two articles and one YouTube video that discuss the constrained subset sum problem:
 
+1. Article: "Subset sum problems with digraph constraints" by Laurent Gourvès, Jérôme Monnot & Lydia Tlilane, published in the Journal of Combinatorial Optimization: 
+https://link.springer.com/article/10.1007/s10878-019-00422-6
+2. Article: "Solutions for subset sum problems with special digraph constraints" by Frank Gurski, Dominique Komander & Carolin Rehs, published in Mathematical Methods of Operations Research: 
+https://link.springer.com/article/10.1007/s00186-020-00722-8
+3. YouTube Video: "1425. Constrained Subset Sum (Leetcode Hard)" by Programming Live with Larry: 
+https://www.youtube.com/watch?v=J0ZbVAynM0Q
+
+I hope these links are helpful! Is there anything else you would like to know? 😊
+"""
+
+
+"""
+Sure! Here are two articles and one YouTube video that discuss the constrained subset sum problem:
+
+1. Article: "Subset sum problems with digraph constraints" by Laurent Gourvès, Jérôme Monnot & Lydia Tlilane, published in the Journal of Combinatorial Optimization¹. This article introduces and studies optimization problems related to the well-known Subset Sum problem, where a node-weighted digraph is given and one has to select a subset of vertices whose total weight does not exceed a given budget while satisfying additional constraints called digraph constraints and maximality.
+2. Article: "Solutions for subset sum problems with special digraph constraints" by Frank Gurski, Dominique Komander & Carolin Rehs, published in Mathematical Methods of Operations Research². This article considers two extensions of the subset sum problem: The subset sum problem with digraph constraint (SSG) and subset sum problem with weak digraph constraint (SSGW), where a digraph with sizes assigned to the vertices is given. The article shows that both problems are NP-hard even on oriented co-graphs and minimal series-parallel digraphs, and provides pseudo-polynomial solutions for SSG and SSGW with digraph constraints given by directed co-graphs and series-parallel digraphs.
+3. YouTube Video: "1425. Constrained Subset Sum (Leetcode Hard)" by Programming Live with Larry⁸. In this video, Larry solves and analyzes the Leetcode problem "Constrained Subset Sum" as both an interviewer and an interviewee. This is a live recording of a real engineer solving a problem live - no cuts or edits!
+
+I hope these resources are helpful! Is there anything else you would like to know? 😊
+
+Source: Conversation with Bing, 8/30/2023
+(1) Subset sum problems with digraph constraints | SpringerLink. https://link.springer.com/article/10.1007/s10878-018-0262-1.
+(2) Solutions for subset sum problems with special digraph constraints .... https://link.springer.com/article/10.1007/s00186-020-00718-6.
+(3) 1425. Constrained Subset Sum (Leetcode Hard) - YouTube. https://www.youtube.com/watch?v=H_H0jEgAdzY.
+(4) Constrained Subsequence Sum - LeetCode. https://leetcode.com/problems/constrained-subsequence-sum/.
+(5) Confusion about dynamic programming on unconstrained subset sum vs .... https://cs.stackexchange.com/questions/131690/confusion-about-dynamic-programming-on-unconstrained-subset-sum-vs-constrained-s.
+(6) Subset Sum Problem using Dynamic Programming | Data Structures and Algorithms. https://www.youtube.com/watch?v=dJmyfFC3-3A.
+(7) 7 Subset Sum Problem. https://www.youtube.com/watch?v=_gPcYovP7wc.
+(8) L-5.5: Sum of Subsets Problem | Dynamic Programming. https://www.youtube.com/watch?v=MLSy2_iQdmY.
+(9) Subset Sum Problem - Explanation and Implementation - YouTube. https://www.youtube.com/watch?v=bH5VkYEEF_8.
+(10) Computer Science: Unconstrained subset sum vs constrained ... - YouTube. https://www.youtube.com/watch?v=BNUkQQvZEyU.
+(11) undefined. https://www.youtube.com/channel/UCM-yUTYGmrNvKOCcAl21g3w/playlists?view_as=subscriber.
+(12) undefined. https://www.geeksforgeeks.org/subset-sum.
+(13) undefined. https://www.youtube.com/@varunainashots.
+(14) undefined. https://leetcode.com/problems/constra.
+(15) undefined. https://www.twitch.tv/larryny.
+(16) undefined. https://discord.gg/6QKRCdR.
+(17) undefined. https://www.instagram.com/larrysomewh.
+(18) undefined. https://github.com/eMahtab/subset-sumFor.
+(19) undefined. https://www.patreon.com/roelvandepaarWith.
+"""
+
+
+"""
+Sure! Here is a list of steps, using non-programming language, to describe the process used to solve the constrained subset sum problem:
+
+1. Start with a list of numbers and a maximum distance value.
+2. Create a new list to keep track of the maximum sums of subsequences that meet the distance constraint.
+3. For each number in the list, calculate the maximum sum of a subsequence that ends with that number and meets the distance constraint.
+4. To do this, first set the current maximum sum to the value of the current number.
+5. Then, find the previous maximum sum that is within the maximum distance from the current number and add it to the current maximum sum.
+6. Keep track of the maximum sums within the maximum distance using a sliding window approach.
+7. After calculating the maximum sums for all numbers in the list, find the largest maximum sum in the list. This is the solution to the problem.
+
+I hope this helps you understand the process used to solve the constrained subset sum problem! Is there anything else you would like to know? 😊
+"""
+
+"""
+The reasoning behind how the maximum sum is updated for the current value being processed is based on the idea of dynamic programming. The function `constrained_subset_sum` calculates the maximum sum of a subsequence of the input list `number_list` such that the distance between any two elements in the subsequence is at most `max_distance`. To do this, the function maintains a list `max_sums` to store the maximum sums of subsequences that meet the distance constraint and end at each index in the input list.
+
+For each index `i` in the input list, the function first sets the current maximum sum to the value of the current number `number_list[i]`. Then, it finds the previous maximum sum that is within `max_distance` from the current index and adds it to the current maximum sum. This is done by checking if the deque `max_sum_indices` is not empty and adding the maximum sum at the first index in the deque to the current maximum sum. The deque `max_sum_indices` is maintained using a sliding window approach to keep track of the maximum sums within `max_distance` from the current index.
+
+By adding the previous maximum sum that is within `max_distance` from the current index to the current maximum sum, we ensure that we are considering all possible subsequences that meet the distance constraint and end at index `i`. This allows us to calculate the maximum sum of a subsequence that meets the distance constraint and ends at each index in the input list.
+
+I hope this explanation helps you understand how and why we update what we add to the current value being processed! Is there anything else you would like to know? 😊
+"""
 
 
 """
